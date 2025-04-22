7ad8f603700b1363779c9109fc1c4861cd0c8205
Siberia55 <omich763@gmail.com>
2025-04-22 14:23:47 +0600

    14:20 22.04 почистил от мусора, добавил иврит для корректной поддержки и теста RTL, часть текстовых строк перместил в отдельный файл, добавил файл констант и перенес туда повторяющися значения,елаю повторный, после отката при случайное слияние

--- app/src/main/java/com/example/marvelheros/ui/components/HeroItem.kt
+++ app/src/main/java/com/example/marvelheros/ui/components/HeroItem.kt
@@ -11,13 +11,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
