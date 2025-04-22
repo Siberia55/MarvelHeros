eba4c25043f7be3a84f20e8662966396aab7325f
Siberia55 <omich763@gmail.com>
2025-04-22 14:38:49 +0600

    14:38 22.04 почистил от мусора, добавил иврит для корректной поддержки и теста RTL, часть текстовых строк перместил в отдельный файл, добавил файл констант и перенес туда повторяющися значения,елаю повторный, после отката при случайное слияние

--- app/src/main/java/com/example/marvelheros/ui/components/FullScreenHeroDetails.kt
+++ app/src/main/java/com/example/marvelheros/ui/components/FullScreenHeroDetails.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.example.marvelheros.R
 import com.example.marvelheros.domain.model.Hero
