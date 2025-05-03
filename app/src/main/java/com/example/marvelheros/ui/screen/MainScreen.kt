fe9edd57a2a39384a134321c10e7a8bac80aba79
Siberia55 <omich763@gmail.com>
2025-05-03 14:08:24 +0600

    добавлена визуализация загрузки данных из API, удалена часть неиспользуемых импортов

--- app/src/main/java/com/example/marvelheros/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/marvelheros/ui/screen/MainScreen.kt
@@ -2,16 +2,13 @@ package com.example.marvelheros.ui.screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
 import com.example.marvelheros.domain.model.Hero
