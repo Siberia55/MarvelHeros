a3aa2ded8daf82b8b972a8ef3b9a4a46ecf405d4
Siberia55 <omich763@gmail.com>
2025-05-26 18:02:52 +0600

    изменено имя файла APK релизной сборки, удалены лишние файдлы и участки кода

--- app/src/main/java/com/example/marvelheros/ui/components/HeroItem.kt
+++ app/src/main/java/com/example/marvelheros/ui/components/HeroItem.kt
@@ -27,8 +27,6 @@ import com.example.marvelheros.ui.theme.diments.Dimens
 fun HeroItem(
     hero: Hero,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-
     ) {
     Box(
         modifier = Modifier
